d2e0a6ffdea58a304e0a5aa8a144707cf463cd9d
Lukas Eck <schuttrim@gmail.com>
2019-12-05 18:33:41 +0100

    last visits visible in HTML

--- docs/js/last-visited.js
+++ docs/js/last-visited.js
@@ -1,8 +1,20 @@
 let lastVisitsCookieName = 'lastVisits';
 
-$(document).ready(function(){
+$(document).ready(async function(){
     addVisit();
+    writeLastVisits();
 });
+
+function writeLastVisits(){
+    let liitems = new Array();
+    let lastVisits = getLastVisits().sort((a, b) =>  new Date(b) - new Date(a));
+    lastVisits = lastVisits.slice(1, lastVisits.length > 11 ? 11 : lastVisits.length) // take last 10 visits without the newest
+
+    lastVisits.forEach(item => {
+        $("#LastVisitsContent").append('<li class="list-group-item">' + moment(item).format("DD.MM.YYYY HH.mm:ss") + '</li>');
+        liitems.push()
+    });
+}
 
 function getLastVisits(){
     let cookie = getCookie(lastVisitsCookieName);
@@ -17,4 +29,8 @@ function addVisit(){
     let array = getLastVisits();
     array.push((new Date()));
     setCookie(lastVisitsCookieName, JSON.stringify(array));
+}
+
+function displayLastVisits(){
+
 }
