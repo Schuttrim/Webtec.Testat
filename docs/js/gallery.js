f4c8a200af88aad64df1b4d11c17c374fc32a3de
Lukas Eck <schuttrim@gmail.com>
2019-12-06 15:33:11 +0100

    gallery implemented

--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -1,9 +1,6 @@
 let gallery = {
-    loader: $('<div />', {class: 'loader'}),
-    container: $('#Gallery-Content'),
     files: ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg'],
-    currentFile: 0,
-    
+    currentFile: 0,    
 }
 
 $(document).ready(function(){
@@ -27,25 +24,45 @@ $('#Gallery-Previous').on('click', function(){
 });
 
 function loadImage(path) {
+    
+    $('#Gallery-Image').attr('src', path);
+
+
+    /*this pole crap doesn't work
+    anyway i'll leave this here to show the effort of non working pole crap....
+
+    why...
+
+    why?
+
     $.ajax({
         type: 'GET',
         url: path,
-        dataType: 'image/jpg',
-        asnyc: false,
+        asnyc: true,
         beforeSend: function(){
             console.log("loading image: ", path);
             //gallery.container.append(gallery.loader);
         },
         success: function(data) {
-            console.log("success", data);
-            $('#Gallery-Image').attr('src', 'data:image/png;base64,'+data);
+            console.log("success");
+            
+            var img = new Image();
+            img.src = 'data:image/jpeg;base64,' +hexToBase64(data);
+
+            document.body.appendChild(img);
+            // $('#Gallery-Content').append(img);
+            $('#Gallery-Image').attr('src', 'data:image/jpeg;base64,'+ hexToBase64(data));
             //gallery.container.find(gallery.loader).remove();
         },
-        error: function(err) {
-            console.log("error",err);
+        error: function(xhr,status,error) {
+            console.log("error",error);
         }
     });
+    hexToBase64() = function(str) {
+    return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));}
+    */
 }
+
 
 function getImagePath(imageName) {
     var getUrl = window.location;
